fb76cf2bdc006ed73a7c191bc488a440ea82b32a
nixkup <nixkup@proton.me>
2026-01-23 10:48:50 -0300

    add: packages

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -21,7 +21,7 @@
       # desenvolvimento
       sqlitebrowser
       jetbrains.idea
-      vscodium
+      zed-editor
 
       # non-free
       vesktop
@@ -40,37 +40,37 @@
   # não estou usando mais
   programs.waybar = {
     enable = true;
-    
+
     settings = {
       mainBar = {
         layer = "top";
         position = "top";
         height = 30;
-        
+
         modules-left = [ "hyprland/workspaces" "hyprland/window" ];
         modules-center = [ "clock" ];
         modules-right = [ "network" "pulseaudio" "tray" ];
-        
+
         "hyprland/workspaces" = {
           format = "{id}";
           on-click = "activate";
         };
-        
+
         "hyprland/window" = {
           max-length = 50;
         };
-        
+
         clock = {
           format = "{:%H:%M}";
           format-alt = "{:%Y-%m-%d}";
           tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
         };
-        
+
         battery = {
           format = "{capacity}% {icon}";
           format-icons = [ "" "" "" "" "" ];
         };
-        
+
         network = {
           format-wifi = "{essid} ({signalStrength}%) ";
           format-ethernet = "{ipaddr}/{cidr} ";
@@ -78,28 +78,28 @@
         };
       };
     };
-    
+
     style = ''
       * {
         font-family: "JetBrains Mono", "Font Awesome 6 Free";
         font-size: 13px;
       }
-      
+
       window#waybar {
         background-color: rgba(191, 14, 235, 0);
         border-bottom: 2px solid rgba(255, 0, 85, 0);
       }
-      
+
       #workspaces button {
         padding: 0 10px;
         color: #cdd6f4;
       }
-      
+
       #workspaces button.active {
         background-color: rgba(163, 6, 211, 0.53);
         color: rgba(255, 255, 255, 1);
       }
-      
+
       #clock, #network {
         padding: 0 10px;
       }
